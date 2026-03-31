b521b2d6e075fa01892f6b50169cd12814891496
Ekaterina Nikulnikova <nikulnikowa2014@gmail.com>
2026-03-31 23:04:50 +0800

    проверка и обработка данных

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@
  */
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
+   const { discount, sale_price, quantity } = purchase;
 }
 
 /**
@@ -17,6 +18,7 @@ function calculateSimpleRevenue(purchase, _product) {
  */
 function calculateBonusByProfit(index, total, seller) {
     // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit } = seller;
 }
 
 /**
@@ -26,13 +28,55 @@ function calculateBonusByProfit(index, total, seller) {
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+    //Проверка входных данных
+    if (!data
+        || !Array.isArray(data.products)
+        || !Array.isArray(data.sellers)
+        || !Array.isArray(data.purchase_records)
+        || (data.products.length === 0)
+        || (data.sellers.length === 0)
+        || (data.purchase_records.length === 0)
+      ) {
+        throw new Error('Некорректные входные данные');
+      }
 
-    // @TODO: Проверка наличия опций
+    //Проверка наличия опций
+    if (!(typeof options === "object")) {
+      throw new Error('Опции не объект');
+    }
 
-    // @TODO: Подготовка промежуточных данных для сбора статистики
+    const { calculateRevenue, calculateBonus } = options;
 
-    // @TODO: Индексация продавцов и товаров для быстрого доступа
+    if (!calculateRevenue || !calculateBonus) {
+      throw new Error('Чего-то не хватает');
+    }
+
+    if (!(typeof calculateRevenue === "function")
+        || !(typeof calculateBonus === "function")
+      ) {
+        throw new Error('Опции не функции');
+      }
+
+    //Подготовка промежуточных данных для сбора статистики
+    const sellerStats = data.sellers.map(seller => ({
+        id: seller.id,
+        name: `${seller.first_name} ${seller.last_name}`,
+        revenue: 0,
+        profit: 0,
+        sales_count: 0,
+        products_sold: {}
+      }));
+
+    //Индексация продавцов и товаров для быстрого доступа
+    const sellerIndex = sellerStats.reduce((result, item) => ({
+      ...result,
+      [item.id]: item
+    }), {});
+
+    const productIndex = data.products.reduce((result, item) => ({
+      ...result,
+      [item.sku]: item
+    }), {});
 
     // @TODO: Расчет выручки и прибыли для каждого продавца
 
@@ -42,3 +86,8 @@ function analyzeSalesData(data, options) {
 
     // @TODO: Подготовка итоговой коллекции с нужными полями
 }
+
+console.log(analyzeSalesData(data, {
+            calculateRevenue: calculateSimpleRevenue,
+            calculateBonus: calculateBonusByProfit
+        }));
